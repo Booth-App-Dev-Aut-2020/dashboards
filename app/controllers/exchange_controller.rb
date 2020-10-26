ad1ac4b6a45a81c6db17b4e34f4e317ed0c4d956
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-26 17:47:28 +0000

    2

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -9,5 +9,25 @@ def first_currency
   render({ :template => "currency_templates/step_one.html.erb"})
 end
 
+def second_currency
+  @first_currency = params.fetch("first_symbol")
+  url="https://api.exchangerate.host/symbols"
+  raw_data = open(url).read
+  parsed_data = JSON.parse(raw_data)
+  symbols_hash = parsed_data.fetch("symbols")
+  @array_symbols = symbols_hash.keys
+  render({ :template => "currency_templates/step_two.html.erb"})
+end
+
+def conversion
+  @first_currency = params.fetch("first_symbol")
+  @second_currency = params.fetch("second_symbol")
+  url="https://api.exchangerate.host/symbols"
+  raw_data = open(url).read
+  parsed_data = JSON.parse(raw_data)
+  symbols_hash = parsed_data.fetch("symbols")
+  @array_symbols = symbols_hash.keys
+  render({ :template => "currency_templates/step_two.html.erb"})
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 get("/forex", { :controller => "exchange", :action => "first_currency"})
+get("/forex/:first_symbol", { :controller => "exchange", :action => "second_currency"})
+get("/forex/:first_symbol/:second_symbol", { :controller => "exchange", :action => "conversion"})
 
 end
